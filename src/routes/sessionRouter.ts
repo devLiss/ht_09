2a56f7de50bc5320cbad56b707e66824bafc24b3
devliss <liseyna1@gmail.com>
2022-10-15 23:08:35 +0300

    debug registration v3

--- src/routes/sessionRouter.ts
+++ src/routes/sessionRouter.ts
@@ -16,8 +16,6 @@ sessionRouter.get('/',async (req:Request, res:Response)=>{
         return
     }
     const sessions = await sessionService.getSessionsByUserId(payload.userId)
-    console.log("GET DEVICES")
-    console.log(sessions)
     res.status(200).send(sessions)
 })
 sessionRouter.delete('/',async (req:Request, res:Response)=>{
