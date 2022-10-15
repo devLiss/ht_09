bbe951e183d050fb389872f7c208612cc8415245
devliss <liseyna1@gmail.com>
2022-10-15 19:31:02 +0300

    Добавлены все методы, кроме ограничения на количество реквестов v7

--- src/application/jwt-service.ts
+++ src/application/jwt-service.ts
@@ -7,7 +7,7 @@ import {tokenRepo} from "../repositories/token-db-repo";
 export const jwtService = {
     async generateTokens(userId:any, deviceId:string){
         const token = jwt.sign({userId:userId}, settings.JWT_SECRET, {expiresIn:'10s'})
-        const refreshToken = jwt.sign({deviceId:deviceId, userId:userId}, settings.JWT_REFRESH_SECRET, {expiresIn:'20s',algorithm:  "RS256"})
+        const refreshToken = jwt.sign({deviceId:deviceId, userId:userId}, settings.JWT_REFRESH_SECRET, {expiresIn:'20s'})
 
         return {
             accessToken:token,
@@ -29,7 +29,7 @@ export const jwtService = {
 
     async getPayloadByRefreshToken(refreshToken:string):Promise<any>{
         try {
-            const result: any = jwt.verify(refreshToken, settings.JWT_REFRESH_SECRET, {algorithms:["RS256"]});
+            const result: any = jwt.verify(refreshToken, settings.JWT_REFRESH_SECRET);
             return result
         }
         catch (e){
