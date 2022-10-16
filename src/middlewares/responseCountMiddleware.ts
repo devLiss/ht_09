a40a78fbecd3977e7cf0b05cba1de69d88300669
devliss <liseyna1@gmail.com>
2022-10-16 15:09:40 +0300

    v5

--- src/middlewares/responseCountMiddleware.ts
+++ src/middlewares/responseCountMiddleware.ts
@@ -8,7 +8,7 @@ export const responseCountMiddleware = async (req:Request, res:Response, next:Ne
     console.log("COUNT "+ count +" ---> URL "+req.url+" ---> IP "+req.ip)
     await requestDbRepo.createRequestRow(req.ip, req.url,new Date())
 
-    if(count > 5){
+    if(count >= 5){
         console.log("Count requests grate then 5!")
         res.sendStatus(429)
         return
